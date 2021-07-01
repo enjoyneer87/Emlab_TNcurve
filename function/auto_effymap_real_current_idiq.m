bb9c3e99696e5eca964b2fde8a56f19b6e0e6e94
enjoyneer87 <dhkang87@g.skku.edu>
2021-07-02 02:22:42 +0900

    시험데이터 실전류를 이용해 Jmag해석을 수행하는 기능_ongoing

--- function/auto_effymap_real_current_idiq.m
+++ function/auto_effymap_real_current_idiq.m
@@ -1,19 +1,23 @@
 function auto_effymap_real_current_idiq(Input)
 
+%% 무슨 입력이지
 Np=8;
 R=0.013149878;
 sf=5500;
 Vdc=360;
-div=1e-6;
+div=1e-6; %해석 sampling ratio
 
+%%역기전력?
 
 f_name = ['Emf_Data_Skew/Lamda_fd@', num2str(Input.base_rpm), '.csv'];
 data = xlsread(f_name);
 lamda = data(1:Input.steps, 2:4);
 
+%%angle
 ang=(0:Input.torque_ripple/Input.steps:Input.torque_ripple-Input.torque_ripple/Input.steps)';
 e_ang=ang*(pi/180)*Input.p/2;       
 
+%%input mode
 if Input.mode_w == -1      %U-W-V
     lamda_d=(2/3)*(lamda(:,1).*cos(e_ang)+lamda(:,2).*cos(e_ang-(2/3)*pi)+lamda(:,3).*cos(e_ang+(2/3)*pi));
 else                     %U-V-W
@@ -22,17 +26,21 @@ end
     
 Lamda_f=mean(lamda_d);
 
+%%effy_map으로부터 PWM 돌릴 skew input
 if Input.skew == 1
     tmp = readcell('Output/Effy_map_skew.csv');
 else
     tmp = readcell('Output/Effy_map.csv');
 end
 
+
 [len a] = size(tmp);
 effydata = cell2mat(tmp(2:(len), 1:a));
 
+%%Simpulink PARAMETER 입력
+
 for i = 1:1:len
-    %% PARAMETER
+
     rpm = effydata(i, 1);
     Torque = effydata(i, 2);
     Imag= effydata(i, 3);
@@ -54,32 +62,37 @@ for i = 1:1:len
     Kiq=R*wc*100;
     Kaq=1/Kpq;
     
-    Total_step = round(1/fre/div);
-    Period_step = round(Total_step/10);
+    Total_step = round(1/fre/div);  %전기각 한주기동안의 전체 스텝
+    Period_step = round(Total_step/10); %이건 뭐지?
     
-    %% EXCUTE
+ 
+    %% Simulink 실행 EXCUTE
     sim('LdLq_Constant_Feedforward_1000');
-    Current_period = Iabc((Total_step-Period_step):(Total_step), 1:3);
     
-    SW_harmonics = sf/fre;
     
-    while SW_harmonics > 25
-        SW_harmonics = SW_harmonics*2;
+    %% Simulink Data export
+    Current_period = Iabc((Total_step-Period_step):(Total_step), 1:3);  %변수명은 전류주기인데 전류 데이터를 그대로 넣네
+    
+    SW_harmonics = sf/fre;  %Switching harmonic 
+    
+    while SW_harmonics > 25 %Swtiching frequency harmonic이 25차 이상일 경우
+        SW_harmonics = SW_harmonics*2;  %왜 두배로 하지?, 변수명 헷갈릴 여지있을듯
     end
     
-    Step_interval = round(Current_period/(4*SW_harmonics));
+    Step_interval = round(Current_period/(4*SW_harmonics)); %step의
     
     Current = Current_period(1:Step_interval:Period_step+1, :);
     
-    Analysis_step = length(Current);
+    Analysis_step = length(Current);    %JMAG에서의 analysis step이겠네 변수명....
 
     time = [0:1/fre/Analysis_step:1/fre-1/fre/Analysis_step 1/fre+0.000001]';
     headers = {'time', 'Ia', 'Ib', 'Ic'};
     Time_Current = [time Current];
     csvwrite_with_headers([num2str(id) '_' num2str(iq) '.csv'], Time_Current, headers);
     
-    %% JMAzdesigner = actxserver('designer.Application.181');
-    designer = actxserver('designer.Application.191');
+%% JMAG Analysis 
+% JMAzdesigner = actxserver('designer.Application.181');
+    designer = actxserver('designer.Application.200');
 %     scheduler = actxserver('scheduler.JobApplication');
     designer.Show();
     app = designer;
@@ -90,7 +103,7 @@ for i = 1:1:len
     app.Load([current_path,'Iron_Loss_K.jproj']);
     app.GetModel(0).GetStudy(0).DeleteResult();
     
-    %% CURRENT SETTING
+%% JMAG CURRENT SETTING
     ia = [time Current(:, 1)];
     ia =arrayfun(@num2str,ia,'un',0);
     app.ShowCircuitGrid(true)
